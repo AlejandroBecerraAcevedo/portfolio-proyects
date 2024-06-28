bcc36331e1b052ddafeffa6cf7ea6c7e844f3037
AlejandroBecerraAcevedo <alejandro.becerraa@udea.edu.co>
2024-06-28 15:31:59 -0500

    touches

--- app/(routes)/services/page.tsx
+++ app/(routes)/services/page.tsx
@@ -1,5 +1,6 @@
 import AvatarServices from "@/components/avatar-services";
 import CircleImage from "@/components/circle-image";
+import ContainerPage from "@/components/container";
 import CoverParticles from "@/components/cover-particles";
 import SliderServices from "@/components/slider-services";
 import TransitionPage from "@/components/transition-page";
@@ -11,32 +12,39 @@ const Services = () => {
             <CoverParticles/>
             <CircleImage/>
             <AvatarServices/>  
-            <div className="grid items-center justify-center 
-                h-screen max-w-5xl gap-6 mx-auto md:grid-cols-2 md:px-20"
-            >
-                <div className="max-w-[450px]">
-                    <h1 className="text-2xl leading-tight text-center 
-                        md:text-left md:text-4xl md:mb-5"
-                    >
-                        Mis{" "}
 
-                        <span className="font-bold text-secondary">
-                            servicios
-                        </span>
+            <ContainerPage>
+                <div className="grid items-center justify-center 
+                    h-screen max-w-5xl gap-6 mx-auto md:grid-cols-2 md:px-20"
+                >
+                    <div className="max-w-[450px]">
+                        <h2 className="text-2xl leading-tight text-center 
+                            md:text-left md:text-4xl md:mb-5"
+                        >
+                            Mis{" "}
 
-                    </h1>
-                    <p className="mb-3 text-xl text-gray-300">
-                        Entre los servicios que ofrezco se encuentran el desarrollo de aplicaciones descentralizadas en ICP con Motoko y Azle, así como el desarrollo de Backend con Spring Boot,
-                        también me especializo en aplicaciones IOT con Arduino, Raspberry Pi y ESP32 como lo son los sistemas de control de piso.
-                    </p>   
-                    <a href="/contact" className="px-3 py-2 rounded-lg bg-secondary 
-                        hover:bg-secondary/65" 
-                    >
-                        Contacta conmigo                    
-                    </a>                
-                </div>
-                <SliderServices/>
-            </div>      
+                            <span className="font-bold text-secondary">
+                                servicios
+                            </span>
+
+                        </h2>
+                        <p className="mb-3 text-xl text-gray-300">
+                            Entre los servicios que ofrezco se encuentran el desarrollo de aplicaciones descentralizadas en ICP con Motoko y Azle, así como el desarrollo de Backend con Spring Boot,
+                            también me especializo en aplicaciones IOT con Arduino, Raspberry Pi y ESP32 como lo son los sistemas de control de piso.
+                        </p>   
+                        <a href="/contact" className="px-3 py-2 rounded-lg bg-secondary 
+                            hover:bg-secondary/65" 
+                        >
+                            Contacta conmigo                    
+                        </a>                
+                    </div>
+                    <SliderServices/>
+                </div>   
+
+
+
+            </ContainerPage>
+               
         </>
     );
 }
--- components/introduction.tsx
+++ components/introduction.tsx
@@ -5,10 +5,11 @@ import { TypeAnimation } from 'react-type-animation';
 
 const Introduction = () => {
     return (
+        
         <div className="z-20 w-full bg-darkBg/60">
-            <div className="z-20 grid items-center h-full p-6 py-20 md:py-0 md:grid-cols-2">
-                <Image src="/home-4.png" priority width="400" height="800" alt="Avatar" />
+            <div className="z-20 grid items-center h-full p-6 py-20 md:py-0 md:grid-cols-2">                
                 <div className="flex flex-col justify-center max-w-md">
+                    <Image src="/home-4.png" width="400" height="800" alt="Avatar" />
                     <h1 className="mb-5 text-2xl leading-tight text-center md:text-left md:text-4xl md:mb-10">Si puedes pensarlo, <br />
                         <TypeAnimation
                             sequence={[
